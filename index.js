2304084339e4d41f8ab26fdb782199c13e870a6f
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-04-29 13:05:01 -0700

    bug fixes code cleanup

--- index.js
+++ index.js
@@ -3,31 +3,14 @@ var todoApp = angular.module('todoApp', ['as.sortable']);
 todoApp.controller('todoCtrl', function($scope, $filter){
 
 	var orderBy = $filter('orderBy');
-	
-	$scope.customOrder = function(a){
-		a = a.priority;
-		if (a == 'high-priority')
-			return 1;
-		if (a == 'medium-priority')
-			return 2;
-		if (a == 'low-priority')
-			return 3;
-	};
 
 	$scope.tasks = [
-		{name: "high-priority", priority: "high-priority"},
-		{name: "low-priority", priority: "low-priority"},
-		{name: "medium-priority", priority: "medium-priority"}
+		{name: "high-priority", priority: 3},
+		{name: "medium-priority", priority: 2},
+		{name: "low-priority", priority: 1}
 	]
 
-	$scope.order = function(predicate, reverse){
-		$scope.predicate = predicate;
-		$scope.reverse = reverse;
-		if (predicate == 'priority')
-			predicate = $scope.customOrder;
-		$scope.tasks = orderBy($scope.tasks, predicate, $scope.reverse)
-	}
-	$scope.order('priority', true);
+	$scope.order = 'priority'
 
 	$scope.addTask = function(){
 		if ($scope.name == undefined || $scope.name == ""){
@@ -35,9 +18,8 @@ todoApp.controller('todoCtrl', function($scope, $filter){
 			return;
 		}
 		$scope.tasks.push({name: $scope.name, priority: $scope.priority});
+		// clear text box
+		$scope.name = "";
 	}
-
-	$scope.priorityLeast = function(){};
-
 });
 
